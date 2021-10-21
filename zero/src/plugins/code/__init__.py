b0318110ff5066942ab541c3f23037d72ac5c061
Anduin9527 <qq3025864569@163.com>
2021-10-22 02:18:54 +0800

    加入执行代码功能

--- zero/src/plugins/code/__init__.py
+++ zero/src/plugins/code/__init__.py
@@ -1,3 +1,4 @@
+from urllib import response
 from nonebot.plugin import on_command, on_keyword, on_message
 from nonebot.rule import to_me
 from nonebot.typing import T_State
@@ -7,6 +8,7 @@ from nonebot.adapters.cqhttp.event import GroupMessageEvent
 from nonebot.adapters._base import Event
 from nonebot.adapters.cqhttp.bot import Bot
 import aiohttp
+import requests
 
 lang_list = on_command("code list", priority=10)
 run = on_command("run", priority=10)
@@ -24,20 +26,45 @@ async def _(bot: Bot, event: Event, state: T_State):
       msg = msg[:-3]
       await lang_list.finish(msg)
 
+langs = ["assembly", "ats", "bash", "c", "clojure", "cobol", "coffeescript", "cpp", "crystal", "csharp", "d", "elixir", "elm", "erlang", "fsharp", "go", "groovy", "haskell",
+         "idris", "java", "javascript", "julia", "kotlin", "lua", "mercury", "nim", "nix", "ocaml", "perl", "php", "python", "raku", "ruby", "rust", "scala", "swift", "typescript"]
+
 
 @run.handle()
 async def _(bot: Bot, event: Event, state: T_State):
+  pass
+
+
+@run.got("lang", prompt="输入语言类型（使用/code list 查看支持语言）")
+async def _(bot: Bot, event: Event, state: T_State):
+  if state["lang"] == "/code list":
+    await run.reject(str(langs))
+  if state["lang"] in langs:
+    pass
+  else:
+    await run.reject("请检查语言名称是否正确")
+
+
+@run.got("content", prompt="输入代码，不排版就(￣ε(#￣)☆╰╮(￣▽￣///)")
+async def _(bot: Bot, event: Event, state: T_State):
   url = "https://glot.io/api/run/"
   headers = {
-      "Authorization": "Token 6f321066-d193-42be-b300-52bd80baf574"
+      'Authorization': 'Token 6f321066-d193-42be-b300-52bd80baf574',
+      'Content-type': 'application/json',
   }
-  content = str(event.get_message())
-  lang = content.split(' ')[0]         # 编程语言
-  content = content[len(lang):].strip()   # 代码内容
-  data = {
-      "files": [{"name": "main.py", "content": "print(123)"}]
-  }
+  content = str(state["content"]).replace('\n', "n").replace(
+      '\r', '\\').replace("&#91;", "[").replace("&#93;", "]")
+  lang = state["lang"]        # 编程语言
+  data = '{"files": [{"name": "main.py", "content": "%(content)s"}]}' % {
+      "content": content}
   url += lang+"/latest"
-  async with aiohttp.ClientSession() as session:
-    async with session.post(url, headers=headers, data=data) as resp:
-      await run.send(str(resp))
+  try:
+    async with aiohttp.ClientSession() as session:
+      async with session.post(url, headers=headers, data=data) as resp:
+        response = await resp.json()
+        if response["stdout"] != "":
+          await run.finish("stdout:\n"+response["stdout"], at_sender=True)
+        else:
+          await run.finish("stderr:\n"+response["stderr"], at_sender=True)
+  except KeyError:
+    await run.finish("执行失败，执行超时或格式错误\n", at_sender=True)
